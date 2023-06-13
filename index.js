f7fc2c62cba5de34f116fe98e646c6e077245f71
Abdul Mannan <abdulmannankhan1000@gmail.com>
2023-06-13 19:29:28 +0500

    ssl

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ import bodyParser from "body-parser";
 import https from 'https'
 import { v4 as uuidv4 } from "uuid";
 import * as fs from "fs";
+import admin from "firebase-admin";
+import serviceAccount from "./pk.json" assert { type: "json" };
+import {getAuth} from "firebase-admin/auth";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let privateKey  = fs.readFileSync('key.pkey', 'utf8');
+let certificate = fs.readFileSync('cert.cer', 'utf8');
+let credentials = {key: privateKey, cert: certificate};
+
+
 
 const DIR = './public/';
 
 const app = express();
+let httpsServer = https.createServer(credentials, app);
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8005;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,7 +33,7 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 app.use('/public', express.static('public'));
 
-app.listen(port,() => {
+httpsServer.listen(port,'0.0.0.0',() => {
   console.log('Connected to port ' + port)
 })
 const storage = multer.diskStorage({
@@ -82,4 +96,9 @@ app.post('/:client', upload.single('image'), async (req, res, next) => {
   // console.log(client);
   // console.log(url + '/public/' + req.file.filename)
   // res.send(url + '/public/' + req.file.filename);
+})
+
+app.delete('/deleteUser/:uid',async function (req, res) {
+  const uid = req.params.uid;
+  await getAuth().deleteUser(uid).then().catch(err => console.log(err))
 })
